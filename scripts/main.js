3a5e8eeb92657c02e44ad39523e9de460eefe491
titushm <titusmeader@btinternet.com>
2026-01-15 20:57:07 +0000

    Fix growth desync when feeding baby animals. Bump ver to 1.0.1 ready for release

--- scripts/main.js
+++ scripts/main.js
@@ -5,13 +5,14 @@ const BEFORE_GROW_INTERVAL = 23900;
 const PARTICLE_OFF = "minecraft:crop_growth_emitter";
 const PARTICLE_ON = "minecraft:heart_particle";
 const SOUND_OFF = "block.copper_bulb.turn_off";
-const SOUND_ON = "block.copper_bulb.turn_on"
+const SOUND_ON = "block.copper_bulb.turn_on";
 const SOUND_OPTIONS = { pitch: 0.4 };
-const EXCLUDED_TYPES = ["minecraft:item", "minecraft:xp_orb"]; 
+const EXCLUDED_TYPES = ["minecraft:item", "minecraft:xp_orb"];
 let INITIALISED = false;
+let NOTIFIED = false;
 
 const CACHE = {
-	tagged: new Set()
+	tagged: new Set(),
 };
 
 function addTagged(entity) {
@@ -24,6 +25,11 @@ function removeTagged(entity) {
 	entity.setDynamicProperty(TAGGED_KEY, false);
 }
 
+function elevatedTrigger(entity, eventId) {
+	system.run(() => {
+		entity.triggerEvent(eventId);
+	});
+}
 
 function triggerBorn() {
 	const entities = Array.from(CACHE.tagged);
@@ -36,11 +42,10 @@ function triggerBorn() {
 	}
 }
 
-
 function displayInteraction(target, particleID, soundID) {
 	const location = target.getHeadLocation();
 	location.y += 0.5;
-	system.run(()=>{
+	system.run(() => {
 		target.dimension.playSound(soundID, location, SOUND_OPTIONS);
 		target.dimension.spawnParticle(particleID, location);
 	});
@@ -52,11 +57,17 @@ world.afterEvents.playerSpawn.subscribe((event) => {
 	INITIALISED = true;
 	triggerBorn();
 	system.runInterval(triggerBorn, BEFORE_GROW_INTERVAL);
-	system.runTimeout(() => world.sendMessage(`§7Baby§2Turt §7Loaded §8[${CACHE.tagged.size} entities tagged]`), 20);
+	system.runTimeout(() => {
+		if (!NOTIFIED) {
+			// Logically this shouldnt be needed but sometimes it duplicates the message.
+			world.sendMessage(`§7Baby§2Turt §7Loaded §8[${CACHE.tagged.size} entities tagged]`);
+			NOTIFIED = true;
+		}
+	}, 20);
 });
 
 // Save entity states
-world.beforeEvents.playerInteractWithEntity.subscribe(({ itemStack, target }) => {	
+world.beforeEvents.playerInteractWithEntity.subscribe(({ itemStack, target }) => {
 	if (!itemStack || itemStack.typeId !== "minecraft:name_tag" || !itemStack.nameTag) return;
 
 	const ageable = target.getComponent("minecraft:ageable");
@@ -64,21 +75,21 @@ world.beforeEvents.playerInteractWithEntity.subscribe(({ itemStack, target }) =>
 
 	if (!target.getDynamicProperty(TAGGED_KEY)) {
 		addTagged(target);
-		target.triggerEvent("minecraft:entity_born");
+		elevatedTrigger(target, "minecraft:entity_born");
 		displayInteraction(target, PARTICLE_ON, SOUND_ON);
-	};
+	}
 });
 
 // Add tagged entities to cache on load
-world.afterEvents.entityLoad.subscribe(({entity}) => {
+world.afterEvents.entityLoad.subscribe(({ entity }) => {
 	if (EXCLUDED_TYPES.includes(entity.typeId)) return;
 	if (entity.getDynamicProperty(TAGGED_KEY)) {
-    	CACHE.tagged.add(entity);
+		CACHE.tagged.add(entity);
 	}
 });
 
 // Clean up cache on entity removal
-world.beforeEvents.entityRemove.subscribe(({removedEntity}) => {
+world.beforeEvents.entityRemove.subscribe(({ removedEntity }) => {
 	if (EXCLUDED_TYPES.includes(removedEntity.typeId)) return;
 	CACHE.tagged.delete(removedEntity);
 });
@@ -86,15 +97,27 @@ world.beforeEvents.entityRemove.subscribe(({removedEntity}) => {
 // Remove tag on name tag use
 world.beforeEvents.itemUse.subscribe((event) => {
 	if (!event.itemStack || event.itemStack.typeId !== "minecraft:name_tag" || event.itemStack.nameTag !== undefined) return;
-	const entities = event.source.getEntitiesFromViewDirection({ maxDistance: 5, includeLiquidBlocks: false, includePassableBlocks: false, excludeTypes: EXCLUDED_TYPES });
+	const entities = event.source.getEntitiesFromViewDirection({
+		maxDistance: 5,
+		includeLiquidBlocks: false,
+		includePassableBlocks: false,
+		excludeTypes: EXCLUDED_TYPES,
+	});
 	let closest = null;
 	for (const entity of entities) {
 		if (EXCLUDED_TYPES.includes(entity.entity.typeId) || !entity.entity.getDynamicProperty(TAGGED_KEY)) continue;
-        if (!closest || entity.distance < closest.distance) closest = entity;
+		if (!closest || entity.distance < closest.distance) closest = entity;
 	}
 	if (!closest) return;
 	removeTagged(closest.entity);
 	displayInteraction(closest.entity, PARTICLE_OFF, SOUND_OFF);
+});
 
+// Reset growth on entity interaction (fixes feeding)
+world.beforeEvents.playerInteractWithEntity.subscribe((event) => {
+	const ageable = event.target.getComponent("minecraft:ageable");
+	if (!ageable) return;
 
+	if (!event.target.getDynamicProperty(TAGGED_KEY)) return; // Ignore as not tagged
+	elevatedTrigger(event.target, "minecraft:entity_born");
 });
